96a73633e8d59d953ec5a3ad14c702a3041bdb22
Robert Butts <robert.o.butts@gmail.com>
2015-08-22 19:14:48 -0500

    Added db.rkt versioning disclaimer

--- db.rkt
+++ db.rkt
@@ -1,3 +1,8 @@
+;; This file has been VERSIONED into the hub repo.
+;; DO NOT change it from hub. Change in github.com/hubski/hubski,
+;; and update the versioned file. Contributors WILL update this
+;; file in hub without checking for changes.
+
 #lang racket
 (require db)
 
